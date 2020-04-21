31cccf38598519b604f5b763e6992d10eb5b4580
vpekarek <v.pekarek@gmail.com>
2020-04-21 09:58:10 +0200

    Modify db.json

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+server.use(jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/reset": "/reset",
+    "/orders/:number": "/orders?number=:number",
+    "/refunds/:number": "/refunds?number=:number",
+    "/faq/:type": "/faq?type=:type"
+}));
+
 server.use(middlewares);
 server.use(router);
 
